04b8a97839bc38ca675289b3e706e76f6862774e
Enrique Ramirez <nrqrmz@gmail.com>
2025-10-15 00:06:37 -0600

    add review and star system

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :restaurants, only: %i[index show new create]
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[create]
+  end
 end
